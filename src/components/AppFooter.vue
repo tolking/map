c9fd974351e4eab71da545a13044202a35f48d62
tolking <qw13131wang@gmail.com>
2020-11-13 21:42:07 +0800

    refactor: init

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <span class="font-12">更新时间</span>
+    <span class="font-12 uptime">2020-11-7</span>
+    <span class="font-12">数据维护</span>
+    <span class="font-12 author">ououe</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+</style>
---
+++ src/components/AppSvg.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="svg" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1024 1024'>
+    <path d='M100 100 L1000 100 L1000 1000 z' style='stroke:#f00;stroke-width: 10'></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AppSvg',
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+.svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
