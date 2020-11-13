cb1bb5eabf11d6eb22b9892d92eaefece8e2f27c
tolking <qw13131wang@gmail.com>
2020-11-13 21:42:07 +0800

    refactor: init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <app-header />
+  <app-svg />
+  <app-footer />
+</template>
+
+<script lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import AppSvg from './components/AppSvg.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppSvg,
+    AppFooter
+  },
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1 class="title">标题</h1>
+    <div class="fun-content"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style>
+.header .title {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
